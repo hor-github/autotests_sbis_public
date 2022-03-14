181c5b10e1e7824b930f4437a68d23d26c04d9f1
hor-github <55850044+hor-github@users.noreply.github.com>
2022-03-14 13:33:00 +0700

    comment deleted

--- conftest.py
+++ conftest.py
@@ -8,8 +8,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture(scope="function")
 def browser(request):
-    # options = Options()
-    # browser = webdriver.Chrome(options=options)
     browser = webdriver.Chrome()
     print("\n! ! ! start browser ! ! !")
     link = 'https://fix-online.sbis.ru/auth/'
@@ -22,22 +20,4 @@ def browser(request):
     browser.quit()
 
 
-# @pytest.fixture(scope="function")
-# def wait_element_clickable():
-#     WebDriverWait.until(EC.element_to_be_clickable())
 
-
-# @pytest.fixture(scope="class")
-# def setup11():
-#     options = Options()
-#     browser = webdriver.Chrome(options=options)
-#     browser = webdriver.Chrome()
-#     print("\n! ! ! start browser ! ! !")
-#     link = 'https://fix-online.sbis.ru/auth/'
-#     page_login = LoginPage(browser, link)
-#     page_login.open()
-#     page_login.authorization(login='Балаган', password='Балаган1234')
-#
-#     yield browser
-#     print("\n! ! ! quit browser ! ! !")
-#     browser.quit()
